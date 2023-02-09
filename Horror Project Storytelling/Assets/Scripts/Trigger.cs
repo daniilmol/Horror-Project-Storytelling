8f794662da86b1961e9e5bcf6e24940f40510113
daniilmol <56126113+daniilmol@users.noreply.github.com>
2023-02-09 13:05:31 -0800

    Fixed trigger and commented code, prefabbed trigger

--- Horror Project Storytelling/Assets/Scripts/Trigger.cs
+++ Horror Project Storytelling/Assets/Scripts/Trigger.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
+    //Moments contain a single game object that's comprised of numerous game objects for the moment (audiosources, particles, ghosts)
+    //Place a moment game object into this trigger for it to occur once the player touches it, always deactivate moments initially
     [SerializeField] GameObject moment;
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player"){
