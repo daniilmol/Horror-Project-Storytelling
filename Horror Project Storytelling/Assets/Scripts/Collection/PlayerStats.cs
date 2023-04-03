4178824e80d256c920087ab95f913a63d9819602
daniilmol <56126113+daniilmol@users.noreply.github.com>
2023-04-03 01:30:46 -0700

    Added RunAt Moment, Player Reset, Ghost Animations, Light Moment, AI Spawning/Despawning, Fixed Sanity

--- Horror Project Storytelling/Assets/Scripts/Collection/PlayerStats.cs
+++ Horror Project Storytelling/Assets/Scripts/Collection/PlayerStats.cs
@@ -18,8 +18,11 @@ public class PlayerStats : MonoBehaviour
     private bool sanityDropping;
     private GlitchEffect playerCamera;
 
+    private Vector3 startingPosition;
+
     public void Start()
     {
+        startingPosition = transform.parent.position;
         numOfSoul = 0;
         numOfKey = 0;
         sanity = 100;
@@ -35,7 +38,17 @@ public class PlayerStats : MonoBehaviour
         setText(numOfSoulText, "Soul: " + numOfSoul.ToString());
         setText(numOfKeyText, "Key: " + numOfKey.ToString());
         ChangePlayerViewBasedOnSanity();
+        PreventNegativeSanity();
     }
+
+    private void PreventNegativeSanity(){
+        if(sanity <= 0){
+            sanityDropRate = 0;
+        }else{
+            sanityDropRate = 1;
+        }
+    }
+
     private void setText(Text text, string numText)
     {
         text.text = numText;
@@ -86,8 +99,16 @@ public class PlayerStats : MonoBehaviour
         sanity += amount;
     }
 
+    public float GetSanity(){
+        return sanity;
+    }
+
     public void ToggleSanityDrain(bool sanityDropping){
         this.sanityDropping = sanityDropping;
+    }
+
+    public Vector3 GetStartingPosition(){
+        return startingPosition;
     }
 
     IEnumerator DropSanityInTheDark(){
--- Horror Project Storytelling/Assets/Scripts/Moments/Moment.cs
+++ Horror Project Storytelling/Assets/Scripts/Moments/Moment.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class Moment : MonoBehaviour
 {
+    public static int numMoments = 0;
+    public static bool runMannequin = false;
     [SerializeField] protected GameObject momentObject;
+    [SerializeField] int required;
+
+    public int GetRequired(){
+        return required;
+    }
 }
